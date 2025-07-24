ff6404f57a8d65dc67f622b96985783f8d4a71c7
codewithhaykay <digitalmagnet506@gmail.com>
2025-07-24 20:09:35 +0100

    Question 4 of assignment2 solved.

--- Question3.py
+++ Question3.py
@@ -1,5 +1,5 @@
 ###This program prints all even numbers between 1 and 20 inclusive
-even_num = []
+even_num = [] #an empty list to collect the numbers
 for x in range(1, 21):
     if x % 2 == 0:
         even_num.append(x)
